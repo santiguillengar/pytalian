09e419501a7c6ca68058b37ce4c9a00e11d7936e
santiguillengar <SantiGuillenGar@c050011.wlan.net.ed.ac.uk>
2017-05-18 21:10:51 +0100

    added verb practice support for 17 verbs, cant add new verbs through system yet

--- pytalian.py
+++ pytalian.py
@@ -78,6 +78,73 @@ def addVocab():
 			if filename != "vocab0.txt":
 				file2.write(italianWord)
 				file2.write(spengWord)
+
+def testVerbs():
+	while (True):
+		filename = getVerb()
+		if filename == "exit":
+			return
+		file = open(filename,"r")
+		correctVerb = []
+		wrongCount = 0
+		wrong = []
+		people = ["Io", "Tu", "Lui/lei", "Noi", "Voi", "Loro"]
+
+		for line in file: 
+			correctVerb.append(line.replace('\n',''))
+		print(correctVerb)
+		print("\n{}".format(filename[:-4]))
+
+		for i in range(len(people)):
+			userPerson = raw_input("{}:   ".format(people[i]))
+			if formatVocab(userPerson) == "exit" or userPerson == "3":
+				file.close()
+				return
+			if (formatVocab(userPerson) != formatVocab(correctVerb[i])):
+				wrongCount += 1
+				print("    Wrong! It should be: {}".format(correctVerb[i]))
+		if wrongCount == 0:
+			print("\n    Perfect!\n")
+
+
+
+def getVerb():
+
+	# (!) I must add the four last verbs when I get home and I have my notes for the code to work properly.
+
+	print("\nVERBS:\n")
+	print("0.  RANDOM")
+	print("1.  ESSERE       11. DIRE")
+	print("2.  AVERE        12. LEGGERE")
+	print("3.  FARE         13. MANGIARE")
+	print("4.  ANDARE       14. VENDERE")
+	print("5.  USCIRE       15. PARTIRE")
+	print("6.  VENIRE       16. FINIRE")
+	print("7.  DARE         17. -")
+	print("8.  BERE         18. -")
+	print("9.  POTERE       19. -")
+	print("10. VOLERE       20. -")
+									
+	verb = raw_input("What verb do you want?   ")
+	verbs = {"0":"RANDOM","1":"ESSERE","2":"AVERE","3":"FARE","4":"ANDARE","5":"USCIRE","6":"VENIRE","7":"DARE",
+	"8":"BERE","9":"POTERE","10":"VOLERE","11":"DIRE","12":"LEGGERE","13":"MANGIARE","14":"VENDERE","15":"PARTIRE",
+	"16":"FINIRE","17":"-","18":"-","19":"-","20":"-"}
+	filename = ""
+
+	while (not(verb in verbs.keys()) and (formatVocab(verb) != "exit")): 
+		verb = raw_input("Please choose a valid verb number:   ")
+		print(verb)
+
+	if verb == "exit":
+		return verb
+	
+	if verb != "0" and str(verbs.get(verb)) != "-":
+		filename = str(verbs.get(verb)) + ".txt"
+	if verb == "0":
+		randnum = random.randint(1, (len(verbs) -1))
+		filename = verbs.get(str(randnum)) + ".txt"
+	return filename
+
 		
 def getUnit():
 
@@ -113,12 +180,16 @@ def formatVocab(vocab):
 def main():
 	print("UoE Foundation Italian 1 revision tool\nMay 2017, @SantiGuillenGar\n")
 	while True:
-		print("\n                               ***** Ciao *****\n")
+		print("\n                            ***** Main menu *****\n")
 		mode = raw_input("To quit press 3 or type \"exit\"\nAre you adding vocab (1) or testing yourself (2)?   ")
 		if mode == "1" or mode == "add":
 	 		addVocab()
 		if mode == "2" or mode == "test":
-			testVocab()
+			testmode = raw_input("Do you want to test yourself on vocab (1) or on verbs (2)?   ")
+			if testmode == "1":
+				testVocab()
+			if testmode == "2":
+				testVerbs()
 		if mode == "3" or mode == "exit":
 			print("\n\nCiao!\n\n")
 			quit()
