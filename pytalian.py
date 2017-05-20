75302320432ef48c8d11dc94c4446fb09fd52b09
santiguillengar <SantiGuillenGar@MacBook-Pro-de-Santi-5.local>
2017-05-20 19:11:25 +0100

    added exercises to practice grammar and passato prossimo verbs

--- pytalian.py
+++ pytalian.py
@@ -2,12 +2,6 @@ import random
 
 def testVocab():
 
-	input_func = None
-	try:
-		input_func = raw_input
-	except NameError:
-		input_func = input
-
 	filename = getUnit()
 	if filename == "exit":
 		return
@@ -33,9 +27,9 @@ def testVocab():
 	random.shuffle(vocab)
 	for item in vocab:
 		print(item[0])
-		translation = input_func("Italian:   ")
+		translation =raw_input("Italian:   ")
 		translation = formatVocab(translation)
-		if (translation != "exit" and translation != "3"):
+		if (translation != "exit" and translation != "-1"):
 			total = total +1
 			if translation != formatVocab(item[1]):
 				print("\n    Nah: {}\n".format(item[1]))
@@ -54,6 +48,7 @@ def testVocab():
 			for error in errors:
 				errorString += error + ", "
 			print("You should revise:\n{}.\n\n".format(errorString[:-2]))
+	file.close()
 
 
 # function used to add new vocabulary to system.
@@ -68,7 +63,7 @@ def addVocab():
 	
 	while True:
 		italianWord = raw_input("\nInsert italian vocab:   ")
-		if italianWord == "exit" or italianWord == "3":
+		if italianWord == "exit" or italianWord == "-1":
 			file.close()
 			if filename != "vocab0.txt":
 				file2.close()
@@ -83,38 +78,130 @@ def addVocab():
 				file2.write(italianWord)
 				file2.write(spengWord)
 
-def testVerbs():
+def testVerbsPresent():
 
 	while (True):
 		filename = getVerb()
 		if filename == "exit":
 			return
+		file = open(filename,"r") #opens file with name of "vocab.txt"
+			
+		correctVerb = []
+		wrongCount = 0
+		wrong = []
+		people = ["Io", "Tu", "Lui/lei", "Noi", "Voi", "Loro"]
+
+		for line in file: 
+			correctVerb.append(line.replace('\n',''))
+
+		print("\n{}".format(filename[:-4]))
+
+		for i in range(len(people)):
+			userPerson = raw_input("{}:   ".format(people[i]))
+			if formatVocab(userPerson) == "exit" or userPerson == "-1":
+				file.close()
+				return
+			if (formatVocab(userPerson) != formatVocab(correctVerb[i])):
+				wrongCount += 1
+				print("    Wrong! It should be: {}".format(correctVerb[i]))
+
+		if wrongCount == 0:
+			print("\n                                    Perfect!\n")
 		else:
-			file = open(filename,"r")
-			correctVerb = []
-			wrongCount = 0
-			wrong = []
-			people = ["Io", "Tu", "Lui/lei", "Noi", "Voi", "Loro"]
+			print("                              You made {} mistakes!".format(wrongCount))
+		file.close()
 
-			for line in file: 
-				correctVerb.append(line.replace('\n',''))
 
-			print("\n{}".format(filename[:-4]))
+def testVerbsPP():
 
-			for i in range(len(people)):
-				userPerson = raw_input("{}:   ".format(people[i]))
-				if formatVocab(userPerson) == "exit" or userPerson == "3":
-					file.close()
-					return
-				if (formatVocab(userPerson) != formatVocab(correctVerb[i])):
-					wrongCount += 1
-					print("    Wrong! It should be: {}".format(correctVerb[i]))
-			if wrongCount == 0:
-				print("\n                                    Perfect!\n")
+	filename = "passatoprossimo.txt"
+	file = open(filename,"r")
+	
+	# PREPARATION OF VOCAB
+	messList = [] 
+	errors = []
+	correct = 0
+	total = 0
+	for line in file:
+		messList.append(line)
+	italian = []
+	speng = []
+	speng = [i.replace('\n','') for i in messList[::2]] #even elements, zero indexed
+	italian = [i.replace('\n','') for i in messList[1::2]] #odd elements, zero indexed
+	verbs = zip(italian, speng)
+	
+	
+	random.shuffle(verbs)
+	for item in verbs:
+		print(item[0])
+		translation = raw_input("Italian:   ")
+		translation = formatVocab(translation)
+		if (translation != "exit" and translation != "-1"):
+			total = total +1
+			if translation != formatVocab(item[1]):
+				print("\n    Nah: {}\n".format(item[1]))
+				if (not formatVocab(item[1]) in errors):
+					errors.append(formatVocab(item[1]))
+				verbs.append(item) #add word to the loop again to be redone
 			else:
-				print("                              You made {} mistakes!".format(wrongCount))
-			file.close()
+				print("\n    Correct!\n")
+				correct = correct + 1 
+		else:
+			break
+	if total != 0:
+		print("\nYou answered {} / {} correctly!".format(correct, total))
+		if (len(errors) != 0):
+			errorString = ""
+			for error in errors:
+				errorString += error + ", "
+			print("You should revise:\n{}.\n\n".format(errorString[:-2]))
+	file.close()
 
+
+def testGrammar():
+
+	filename = "grammar.txt"
+	file = open(filename,"r")
+	
+	# PREPARATION OF VOCAB
+	messList = [] 
+	errors = []
+	correct = 0
+	total = 0
+	for line in file:
+		messList.append(line)
+	italian = []
+	speng = []
+	speng = [i.replace('\n','') for i in messList[::2]] #even elements, zero indexed
+	italian = [i.replace('\n','') for i in messList[1::2]] #odd elements, zero indexed
+	grammar = zip(italian, speng)
+	
+	
+	random.shuffle(grammar)
+	for item in grammar:
+		print(item[0])
+		translation = raw_input("Italian:   ")
+		translation = formatVocab(translation)
+		if (translation != "exit" and translation != "-1"):
+			total = total +1
+			if translation != formatVocab(item[1]):
+				print("\n    Nah: {}\n".format(item[1]))
+				if (not formatVocab(item[1]) in errors):
+					errors.append(formatVocab(item[1]))
+				grammar.append(item) #add word to the loop again to be redone
+			else:
+				print("\n    Correct!\n")
+				correct = correct + 1 
+		else:
+			break
+	if total != 0:
+		print("\nYou answered {} / {} correctly!".format(correct, total))
+		if (len(errors) != 0):
+			errorString = ""
+			for error in errors:
+				errorString += error + ", "
+			print("You should revise:\n{}.\n\n".format(errorString[:-2]))
+	file.close()
 
 
 def getVerb():
@@ -137,21 +224,20 @@ def getVerb():
 	"8":"BERE","9":"POTERE","10":"VOLERE","11":"DIRE","12":"LEGGERE","13":"MANGIARE","14":"VENDERE","15":"PARTIRE",
 	"16":"FINIRE","17":"CHIAMARSI","18":"PREFERIRE","19":"STARNUTIRE","20":"CHIUDERE", "21":"APRIRE", "22":"ALZARSI",
 	"23":"TORNARE"}
-	
-	filename = ""
 
-	while (not(verb in verbs.keys()) and (formatVocab(verb) != "exit")): 
+
+	while (not(verb in verbs.keys()) and (formatVocab(verb) != "exit") and (formatVocab(verb) != "-1") ): 
 		verb = raw_input("Please choose a valid verb number:   ")
 		print(verb)
 
-	if verb == "exit":
+	if verb == "exit" or verb == "-1":
 		filename = "exit"
-	if verb == "0":
-		randnum = random.randint(1, (len(verbs) -1))
-		filename = verbs.get(str(randnum)) + ".txt"
-	if verb != "0":
-		filename = str(verbs.get(verb)) + ".txt"
-
+	else:
+		if verb == "0":
+			randnum = random.randint(1, (len(verbs) -1))
+			filename = verbs.get(str(randnum)) + ".txt"
+		else:
+			filename = str(verbs.get(verb)) + ".txt"
 	return filename
 
 
@@ -186,9 +272,9 @@ def getUnit():
 def translate():
 	print("\nTRANSLATION")
 	lang = raw_input("Is your word in italian (1) or in english (2)?   ")
-	while (lang != "1" and lang!="2" and lang !="3" and lang!= "exit"): 
+	while (lang != "1" and lang!="2" and lang !="-1" and lang!= "exit"): 
 		lang = raw_input("Please select language of your word: italian (1) or english (2):   ")
-	if lang == "3" or lang == "exit":
+	if lang == "-1" or lang == "exit":
 		return
 
 	word = raw_input("Insert word to translate:   ")
@@ -238,18 +324,28 @@ def main():
 	print("UoE Foundation Italian 1 revision tool\nMay 2017, @SantiGuillenGar\n")
 	while True:
 		print("\n                            ***** Main menu *****\n")
-		mode = raw_input("To quit press 3 or type \"exit\"\nAre you translating something (0), adding vocab (1) or testing yourself (2)?   ")
-		if mode == "0" or mode == "translate":
-			translate()
+		mode = raw_input("To quit type \"-1\" or type \"exit\"\nAre you adding vocab (1), testing yourself (2) or translating something (3)?   ")
 		if mode == "1" or mode == "add":
 	 		addVocab()
 		if mode == "2" or mode == "test":
-			testmode = raw_input("Do you want to test yourself on vocab (1) or on verbs (2)?   ")
+			testmode = raw_input("Do you want to test yourself on vocab (1), on verbs (2) or on grammar (3)?   ")
 			if testmode == "1":
 				testVocab()
 			if testmode == "2":
-				testVerbs()
-		if mode == "3" or mode == "exit":
+				verbmode = raw_input("Present (1) or passato prossimo (2)?   ")
+				if verbmode == "1": 
+					testVerbsPresent()
+				if verbmode == "2":
+					testVerbsPP()
+				if verbmode == "-1" or verbmode == "exit":
+					mode == "-1"
+				else:
+					print("Error: That verb tense is not available!") 
+			if testmode == "3":
+				testGrammar()
+		if mode == "3" or mode == "translate":
+			translate()
+		if mode == "-1" or mode == "exit":
 			print("\n\nCiao!\n\n")
 			quit()
 
